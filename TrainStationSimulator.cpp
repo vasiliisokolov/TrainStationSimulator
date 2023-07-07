2831572c0d09270d676a2567932affe6033e8430
Basileus <Basileus@DESKTOP-M14F8KM>
2023-07-07 21:18:45 +0300

    change class

--- TrainStationSimulator.cpp
+++ TrainStationSimulator.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 
 std::mutex staition_access;
+std::vector<Train*> trains;
+std::vector<std::thread> railways;
 
 class Train
 {
@@ -29,9 +31,13 @@ public:
         }
         else
         {
+            char answer;
             staition_access.lock();
             station->setTrain(this);
             //staition_access.unlock();
+            std::cout << "Train " << litera << " requesting permission to send!(Y/N)";
+            std::cin >> answer;
+            if(answer == 'Y') station->departTrain();
         }
     }
     
@@ -76,6 +82,37 @@ public:
 int main()
 {
     std::cout << "Railway Station simulation!\n";
+    RailwayStation* station = new RailwayStation();
 
+    for (int i = 0; i < 3; i++)
+    {
+        char tempChar;
+        int tempTime;
+        std::cout << "Enter a train's litera: ";
+        std::cin >> tempChar;
+        std::cout << "Enter a time to arrive: ";
+        std::cin >> tempTime;
+        Train* train = new Train(tempChar, tempTime);
+        trains.push_back(train);
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        railways.emplace_back(&Train::trip, trains[i], station);
+    }
+
+    while(station->count > 3)
+    for (int i = 0; i < 3; i++)
+    {
+        railways[i].join();
+    }
+
+    
+
+    for (int i = 2; i >= 0; i--)
+    {
+        delete trains[i];
+    }
+    delete station;
 }
 
